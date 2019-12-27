63195c650c8325e96f7b364f7f522b789355db3d
steadylearner <32325099+steadylearner@users.noreply.github.com>
2019-12-27 14:47:08 -0500

    Update user.sql for grpc and microservices/tonic_server. I was using the wrong type for examples before. I investigated it with \d+ users option and could find the problem.

--- microservices/tonic_server/user.sql
+++ microservices/tonic_server/user.sql
@@ -1,27 +1,31 @@
 -- http://www.postgresqltutorial.com/postgresql-uuid/
 -- CREATE DATABASE grpc OWNER you;
 -- \c grpc;
--- CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 -- SELECT uuid_generate_v4();
 
 CREATE TABLE users (
-    id uuid PRIMARY KEY DEFAULT uuid_generate_v4 (),
+    -- use VARCHAR instead of uuid because of the type problem.
+    -- Should find the better way and modify the code later if necessary.
+    -- It is also easy to test end points with this.
+    id VARCHAR(255) PRIMARY KEY DEFAULT uuid_generate_v4 (),
     first_name VARCHAR(60) NOT NULL,
     last_name VARCHAR(60) NOT NULL,
     date_of_birth Date NOT NULL
 );
 
 INSERT INTO users
-    (first_name, last_name, date_of_birth)
+    (id, first_name, last_name, date_of_birth)
 VALUES
-    ('steady', 'learner', '2019-01-01');
+    ('steadylearner', 'steady', 'learner', '2019-01-01');
 
 INSERT INTO users
-    (first_name, last_name, date_of_birth)
+    (id, first_name, last_name, date_of_birth)
 VALUES
-    ('mybirthdayis', 'blackfriday', '2019-11-25');
+    ('mybirthdayisblackfriday', 'mybirthdayis', 'blackfriday', '2019-11-25');
 
 INSERT INTO users
-    (first_name, last_name, date_of_birth)
+    (id, first_name, last_name, date_of_birth)
 VALUES
-    ('mybirthdayis', 'notblackfriday', '2019-11-26');
+    ('mybirthdayisnotblackfriday', 'mybirthdayis', 'notblackfriday', '2019-11-26');
+
