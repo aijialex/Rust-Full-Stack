8cef8fce0a985eef6a8abc24197980879a2ae46d
steadylearner <32325099+steadylearner@users.noreply.github.com>
2020-01-12 06:50:47 -0500

    Include libp2p JavaScript example. Should make Rust examples after you learn   futures in Rust.

---
+++ async/block_on/src/main.rs
@@ -0,0 +1,12 @@
+extern crate futures;
+use futures::executor::block_on;
+
+async fn hello_world() {
+    println!("hello, world!");
+}
+
+fn main() {
+    let future = hello_world(); // Nothing is printed
+    println!("Show me first");
+    block_on(future); // `future` is run and "hello, world!" is printed
+}
