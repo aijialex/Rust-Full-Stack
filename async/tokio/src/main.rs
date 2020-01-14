96b7fdbe7f8dd7d4613b97dc187d3da8780e7443
steadylearner <32325099+steadylearner@users.noreply.github.com>
2020-01-14 05:41:20 -0500

    Update tokio ehco-server example.

--- async/tokio/src/main.rs
+++ async/tokio/src/main.rs
@@ -3,20 +3,38 @@ extern crate tokio;
 use tokio::net::TcpListener;
 use tokio::prelude::*;
 
+use std::env;
+use std::error::Error;
+
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut listener = TcpListener::bind("127.0.0.1:8080").await?;
+async fn main() -> Result<(), Box<dyn Error>> {
+    // Allow passing an address to listen on as the first argument of this
+    // program, but otherwise we'll just set up our TCP listener on
+    // 127.0.0.1:8080 for connections.
+    let target = env::args()
+        .nth(1)
+        .unwrap_or_else(|| "127.0.0.1:8080".to_string());
+
+    let mut listener = TcpListener::bind(&target).await?;
+    println!("Listening on: {}", &target);
 
     loop {
+        // async wait for an inbound socket.
         let (mut socket, _) = listener.accept().await?;
+
+        // This is where much of the magic of this server happens. We
+        // crucially want all clients to make progress concurrently, rather than
+        // blocking one on completion of another. To achieve this we use the
+        // `tokio::spawn` function to execute the work in the background.
+        //
+        // Essentially here we're executing a new task to run concurrently,
+        // which will allow all of our clients to be processed concurrently.
 
         tokio::spawn(async move {
             let mut buf = [0; 1024];
 
             // In a loop, read data from the socket and write the data back.
             loop {
-                // $curl localhost:8080
-                // println!("Someone sent a request.");
                 let n = match socket.read(&mut buf).await {
                     // socket closed
                     Ok(n) if n == 0 => return,
@@ -31,6 +49,9 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                 if let Err(e) = socket.write_all(&buf[0..n]).await {
                     eprintln!("failed to write to socket; err = {:?}", e);
                     return;
+                } else {
+                    // $curl localhost:8080
+                    println!("Someone sent a request.");
                 }
             }
         });
