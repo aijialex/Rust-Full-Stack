8b1eea12c8dff60996993007598bf7e8137f9cfc
steadylearner <32325099+steadylearner@users.noreply.github.com>
2020-01-14 12:19:27 -0500

    Update async_std/src/main.rs

--- async/async_std/src/main.rs
+++ async/async_std/src/main.rs
@@ -6,17 +6,17 @@ use async_std::task;
 
 use std::time::Duration;
 
-async fn renew_sitemap_each_day() {
-    // Refer to this.
+async fn renew_sitemaps_each_day() {
+    // Refer to this and make a microservice for simtemaps of your website.
     // https://github.com/steadylearner/Rust-Full-Stack/tree/master/sitemap
 
     let day_in_secs = 1; // Use 1 for testing, 86400 for production.
     let mut interval_day = stream::interval(Duration::from_secs(day_in_secs));
     while let Some(_) = interval_day.next().await {
-        println!("Renew sitemaps for each day.");
+        println!("Renew sitemaps each day.");
     }
 }
 
 fn main() {
-    task::block_on(renew_sitemap_each_day())
+    task::block_on(renew_sitemaps_each_day())
 }
